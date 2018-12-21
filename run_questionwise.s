bacfd19d425add4e68b4926cbd6f0485ac44766c
amala2895 <amala1105@gmail.com>
2018-12-20 23:54:36 -0500

    batch script change:

--- run_questionwise.s
+++ run_questionwise.s
@@ -12,4 +12,6 @@
 
 module load anaconda3/5.3.1
 source activate vision
-python GalaxyZooQuestionsWise.py --data data_augmented --model_directory models_questionwise --validation_length 30000 --train_length 150000 --outputfile output_question.txt --augmentation yes 
+python GalaxyZooQuestionsWise.py --data data_augmented --model_directory models_questionwise --validation_length 30000 --train_length 150000 --outputfile output_question.txt --augmentation yes --epochs 100 --batch_size 128
+
+ 
